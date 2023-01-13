9a15afeb340aa012cc7398c6bce974acffe6739a
sruthi-dasari <113626139+sruthi-dasari@users.noreply.github.com>
2023-01-13 19:09:35 +0530

    Update index.js

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -12,21 +12,21 @@ class NavBar extends Component {
           alt="website logo"
           className="website-logo"
         />
-        <div className="score-and-timer-container">
-          <div className="score-container">
+        <ul className="score-and-timer-container">
+          <li className="score-container">
             <p className="score-title">
               Score: <span className="score-text">{score}</span>
             </p>
-          </div>
-          <div className="timer-container">
+          </li>
+          <li className="timer-container">
             <img
               src="https://assets.ccbp.in/frontend/react-js/match-game-timer-img.png"
               alt="timer"
               className="timer"
             />
             <p className="time-text">{seconds} sec</p>
-          </div>
-        </div>
+          </li>
+        </ul>
       </li>
     )
   }
